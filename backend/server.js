dc419ce6df2cac13bc38aa51461cbba26ed9558f
rebeacham14 <70616503+rebeacham14@users.noreply.github.com>
2025-07-16 12:59:35 -0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+const ticketRoutes = require('./routes/ticketRoute')
+const seatRoutes = require('./routes/seatRoute')
+const userRoutes = require('./routes/userRoute')
+const fighterRoutes = require('./routes/fighterRoute')
+
+
+
+// express app
+const SirRocPromotions = express()
+
+
+// middleware
+SirRocPromotions.use(express.json())
+
+SirRocPromotions.use((req, res, next) => {  
+    console.log(req.path, req.method)
+    next()
+}) 
+
+
+
+SirRocPromotions.use(express.json())
+
+
+
+// routes
+SirRocPromotions.use('/api/ticket/', ticketRoutes)
+SirRocPromotions.use('/api/user/', userRoutes)
+SirRocPromotions.use('/api/fighter/', fighterRoutes)
+SirRocPromotions.use('/api/seat/', seatRoutes)
+
+
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        
+        
+        // listen requests
+        SirRocPromotions.listen(process.env.PORT, () => {
+            console.log('SirRoc Promotions server is connected to database and running on port ' + process.env.PORT)
+        })        
+                
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err)
+    })
+
+
+

8f78e8a0816f23f7cb66d94fb81fe2b184766f96
rebeacham14 <70616503+rebeacham14@users.noreply.github.com>
2025-08-05 15:01:13 -0700

    Update index.js
    update variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './SirRocApp';
+import SirRocApp from './SirRocApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SirRocApp />
   </React.StrictMode>
 );

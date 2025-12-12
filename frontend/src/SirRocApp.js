034453a626c6f028927d5b591dc16539e29471b1
rebeacham14 <70616503+rebeacham14@users.noreply.github.com>
2025-12-11 19:25:40 -0800

    Add new routes and components to SirRocApp

--- frontend/src/SirRocApp.js
+++ frontend/src/SirRocApp.js
@@ -2,10 +2,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 // pages and components
 import Home from './pages/Home/Home.js';
+import Events from './pages/Events/Events.js';
+import Fighters from './pages/Fighters/Fighters.js';
+import OurCrew from './pages/OurCrew/OurCrew.js';
 import BuyATicket from './pages/BuyATicket/BuyATicket.js';
 import PaymentSuccess from './pages/PaymentSuccess/PaymentSuccess.js';
+import Scanner from './pages/Scanner/Scanner.js';
 import AdminView from './pages/AdminView/AdminView.js';
-
 import NavBar from './components/NavBar/NavBar.js';
 
 
@@ -15,17 +18,21 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <NavBar />  */}
+        <NavBar /> 
 
         <div className="pages">
           <Routes>
             
             {/* <Route path="/" element={<Login /> } /> */}
             <Route path="/" element={<Home /> } />
+
+            <Route path="/events" element={<Events /> } />
+            <Route path="/fighters" element={<Fighters /> } />
+            <Route path="/our-crew" element={<OurCrew /> } />
             <Route path="/buyaticket" element={<BuyATicket /> } />
             <Route path="/payment-success/" element={<PaymentSuccess /> } />
             <Route path="/admin-view/" element={<AdminView /> } />
-            
+            <Route path="/scanner/" element={<Scanner /> } />
             </Routes>
         </div>
         {/* <Footer /> */}
